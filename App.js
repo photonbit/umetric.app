e957ae4a3223e801072fa59577d0ec346436f4ae
Daniel Moreno <dani@photonbit.es>
2021-01-22 18:15:35 +0100

    Mock screens for input flow

--- App.js
+++ App.js
@@ -15,24 +15,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import CompleteFlow from './navigation/CompleteFlow';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CompleteFlow />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
