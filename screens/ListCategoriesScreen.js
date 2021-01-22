db9fb5df1548baa1a13c199947ee9e799582ad18
Daniel Moreno <dani@photonbit.es>
2021-01-22 18:15:35 +0100

    Mock screens for input flow

--- screens/ListCategoriesScreen.js
+++ screens/ListCategoriesScreen.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+
+import * as RootNavigation from '../navigation/RootNavigation';
+import Icon from '../components/Icon';
 
 export default function ListCategoriesScreen() {
+	const onPress = () => RootNavigation.navigate("ListEvents");
 	return (
-		<View>
-			<Text>ListCategoriesScreen</Text>
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="mountain" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="bed" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="bad_habits" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="learning" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="tennis" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="creativity" />
+			</TouchableOpacity>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		flex: 1,
+		flexWrap: 'wrap',
+		flexDirection: 'row',
+		paddingTop: 50,
+	},
+	element: {
+		width: '50%',
+		padding: '5%',
+		height: '25%',
+	}
+		
 });
 
--- screens/ListEventsScreen.js
+++ screens/ListEventsScreen.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import * as Linking from 'expo-linking';
 
-export default function ListEventsScreen() {
+import * as RootNavigation from '../navigation/RootNavigation';
+import Icon from '../components/Icon';
+
+export default function ListCategoriesScreen() {
+	const onPress = () => Linking.openURL("https://open.spotify.com/playlist/7pGSSU5UcIyBVNSB3DEYbD?si=ztYl8uZHTKS7_UsnkNKQqQ");
 	return (
-		<View>
-			<Text>ListEventsScreen</Text>
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="meditation" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="egg" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="plank" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="reading" />
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.element} onPress={onPress}>
+					<Icon icon="hygiene_products" />
+			</TouchableOpacity>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		flex: 1,
+		flexWrap: 'wrap',
+		flexDirection: 'row',
+		paddingTop: 50,
+	},
+	element: {
+		width: '50%',
+		padding: '5%',
+		height: '25%',
+	}
+		
 });
 
