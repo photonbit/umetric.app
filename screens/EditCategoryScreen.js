61469b2a32917bb9a790a176ee80b80936953b0f
Daniel Moreno <dani@photonbit.es>
2021-02-11 11:35:20 +0100

    Icon selector draft

--- screens/EditCategoryScreen.js
+++ screens/EditCategoryScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, FlatList, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import t from 'tcomb-form-native';
 
+import * as Icons from '../assets/icons';
 import Icon from '../components/Icon';
 import * as RootNavigation from '../navigation/RootNavigation';
 
@@ -30,10 +31,57 @@ export default function EditCategoryScreen() {
         action: t.maybe(t.String)
     });
 
+    const [modalVisible, setModalVisible] = useState(false);
+    const iconNames = Object.keys(Icons);
+    console.log(iconNames);
+
+    const renderItem = ({item}) => (
+		<TouchableOpacity onPress={() => {ejemplo.icon = item; setModalVisible(false);}} >
+		<View style={styles.icon}>
+            <Icon icon={item} />
+        </View>
+		</TouchableOpacity>
+	);
+
 	return (
 		<View style={styles.container}>
+		    <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                    <Text style={styles.modalText}>Elije el icono</Text>
+                    <FlatList
+                        style={styles.flatlist}
+                        data={iconNames}
+                        renderItem={renderItem}
+                        horizontal={false}
+                        numColumns={3}
+                        keyExtractor={item => item}
+                    />
+
+                    <TouchableOpacity
+                      style={styles.button}
+                      underlayColor='#99d9f4'
+                      onPress={() => {
+                        setModalVisible(!modalVisible);
+                      }}>
+                      <Text style={styles.textStyle}>Me quedo con el que estaba</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+            </Modal>
+
 		    <Form type={Category} options={formOptions} value={ejemplo} />
-			<TouchableOpacity style={styles.icon} onPress={onPress} >
+			<TouchableOpacity
+			    style={styles.icon}
+			    onPress={() => {
+                  setModalVisible(true);
+                }} >
 				<Icon icon={ejemplo.icon} />
 			</TouchableOpacity>
 			<TouchableOpacity style={styles.button} onPress={onPress} underlayColor='#99d9f4'>
@@ -51,7 +99,6 @@ const styles = StyleSheet.create({
 	},
 	container: {
     justifyContent: 'center',
-    marginTop: 50,
     padding: 20,
     backgroundColor: '#ffffff',
   },
@@ -69,6 +116,53 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     alignSelf: 'stretch',
     justifyContent: 'center'
-  }
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  flatlist: {
+    flex: 1,
+    marginBottom: 10
+  },
+  icon: {
+    height: 90,
+    width: 90,
+    padding: 10
+  },
 });
 
