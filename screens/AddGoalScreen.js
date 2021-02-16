2ea97c78b11920b87ca0588685320b9f6ef989bb
Daniel Moreno <dani@photonbit.es>
2021-02-16 11:59:04 +0100

    Add goal mock

--- screens/AddGoalScreen.js
+++ screens/AddGoalScreen.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import NumericInput from 'react-native-numeric-input';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import { Feather } from '@expo/vector-icons';
 
 import CategorySelector from '../components/CategorySelector';
 import Element from '../components/Element';
@@ -19,7 +19,7 @@ export default function AddGoalScreen() {
 			"name": "Rumiaciones",
 			"icon": "programation",
     });
-    const [number, setNumber] = useState(1);
+    const [number, setNumber] = useState("1");
     const [unit, setUnit] = useState(1)
 
 	return (
@@ -31,22 +31,39 @@ export default function AddGoalScreen() {
             setCategory={setCategory}
            />
           <Text style={styles.title}>Quieres dedicar</Text>
-          <NumericInput
-            value={number}
-            onChange={setNumber}
-            minValue={1}
-            rounded
-          />
-          <SegmentedControl
-            values={['veces', 'horas']}
-            selectedIndex={unit}
-            onChange={ (event) => {
-              setUnit(event.nativeEvent.selectedSegmentIndex);
-            }}
-          />
-           <View style={styles.category}>
+          <View style={styles.numberSelection}>
+            <TouchableOpacity
+                onPress={ () => setNumber( (Number(number) - 1).toString())}
+                style={styles.numberButton}
+            >
+                <Feather name="minus-circle" size={40} color="black" />
+            </TouchableOpacity>
+            <TextInput
+                onChangeText={setNumber}
+                defaultValue={number}
+                style={styles.numberInput}
+                keyboardType="number-pad"
+            />
+            <TouchableOpacity
+                onPress={ () => setNumber( (Number(number) + 1).toString())}
+                style={styles.numberButton}
+            >
+                <Feather name="plus-circle" size={40} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.kindSelection}>
+              <SegmentedControl
+                style={styles.kindInput}
+                fontStyle={styles.kindInputText}
+                values={['veces', 'horas']}
+                selectedIndex={unit}
+                onChange={ (event) => {
+                  setUnit(event.nativeEvent.selectedSegmentIndex);
+                }}
+              />
+          </View>
+           <View style={styles.categorySelection}>
 		    <Text style={styles.title}>a la semana a:</Text>
-
                 <Element
                     element={category}
                     onPress={() => {setModalVisible(true);}}  />
@@ -80,13 +97,14 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 8,
         marginBottom: 10,
-        marginTop: 10,
+        marginTop: 25,
         alignSelf: 'stretch',
         justifyContent: 'center'
     },
     title: {
         fontSize: 18,
         fontWeight: 'bold',
+        marginBottom: 15
     },
     input: {
         fontSize: 18,
@@ -103,8 +121,43 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         marginTop: 5,
     },
-    category: {
-        height: 200,
+    numberSelection: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 80,
+    },
+    numberInput: {
+        fontSize: 20,
+        height: 50,
+        width: 50,
+        borderColor: '#CCCCCC',
+        backgroundColor: '#FAFAFA',
+        color: '#111111',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        marginBottom: 10,
+        marginTop: 5,
+    },
+    numberButton: {
+        padding: 15
+    },
+    kindSelection: {
+        height: 70,
+        marginTop: 15
+    },
+    kindInput: {
+        height: 50
+    },
+    kindInputText: {
+        fontSize: 18
+    },
+    categorySelection: {
+        height: 180,
     }
 });
 
