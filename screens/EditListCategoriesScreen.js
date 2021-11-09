d20aca89cf5a39257dc1254b56c81951692e3075
Daniel Moreno <daniel.moreno@tesselo.com>
2021-11-09 22:22:23 +0000

    feat: edit lists load real data now

--- screens/EditListCategoriesScreen.js
+++ screens/EditListCategoriesScreen.js
@@ -1,38 +1,26 @@
 import React, { useLayoutEffect } from 'react'
-import { Alert, FlatList, StyleSheet, TouchableOpacity } from 'react-native'
+import {Alert, FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 import EditableElement from '../components/EditableElement'
 import * as RootNavigation from '../navigation/RootNavigation'
+import {useQuery} from "react-query";
+import {getCategories} from "../services/UmetricAPI";
 
 export default function EditListCategoriesScreen ({ navigation }) {
-  const categorias = [
-    {
-      id: '1',
-      name: 'Rutinas matutinas',
-      icon: 'mountain'
-    },
-    {
-      id: '2',
-      name: 'Rutinas nocturnas',
-      icon: 'bed'
-    },
-    {
-      id: '3',
-      name: 'Comportamientos evitativos',
-      icon: 'bad_habits'
-    },
-    {
-      id: '4',
-      name: 'Deportes',
-      icon: 'tennis'
-    }
-  ]
 
-  const onNamePress = () => RootNavigation.navigate('ListEditEvents')
-  const onEditPress = () => RootNavigation.navigate('EditCategory')
-  const onDeletePress = () => Alert.alert(
-    'Borrar Categoría',
+  const { data, error, isError, isLoading } = useQuery('categories', getCategories)
+  if (isLoading) {
+    return <View><Text>Loading...</Text></View>
+  }
+  if (isError) {
+    return <View><Text>Something is wrong: {error.message}...</Text></View>
+  }
+
+  const onNamePress = (item) => RootNavigation.navigate('ListEditEvents', { category_id: item.id })
+  const onEditPress = (item) => RootNavigation.navigate('EditCategory', { category_id: item.id })
+  const onDeletePress = (item) => Alert.alert(
+    'Borrar ' + item.name + '?',
     'Que la vas a borrar',
     [
       {
@@ -58,16 +46,16 @@ export default function EditListCategoriesScreen ({ navigation }) {
   const renderItem = ({ item }) => (
     <EditableElement
     element={item}
-    onNamePress={onNamePress}
-    onEditPress={onEditPress}
-    onDeletePress={onDeletePress} />
+    onNamePress={() => onNamePress(item)}
+    onEditPress={() => onEditPress(item)}
+    onDeletePress={() => onDeletePress(item)} />
   )
   return (
           <FlatList
       style={styles.flatlist}
-      data={categorias}
+      data={data}
       renderItem={renderItem}
-      keyExtractor={item => item.id}
+      keyExtractor={item => item.id.toString()}
     />
   )
 }
