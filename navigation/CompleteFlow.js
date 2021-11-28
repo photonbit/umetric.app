fadff0807cfdfbe207ef8ea6d0b77982d6b665d3
Daniel Moreno <daniel.moreno@tesselo.com>
2021-11-28 15:14:21 +0000

    feat: store login cookie

--- navigation/CompleteFlow.js
+++ navigation/CompleteFlow.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer'
+
 import { createStackNavigator } from '@react-navigation/stack'
 
 import LoginScreen from '../screens/LoginScreen'
@@ -12,12 +13,28 @@ import { RemindersFlow } from './RemindersFlow'
 import { navigationRef } from './RootNavigation'
 
 import { Context } from '../filters/Store'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 const Drawer = createDrawerNavigator()
 const Stack = createStackNavigator()
 
 export default function CompleteFlow () {
-  const [state] = useContext(Context)
+  const [state, dispatch] = useContext(Context)
+
+  function logout() {
+      AsyncStorage.removeItem("remember_cookie").then(function() {
+            axios.defaults.headers.common["cookie"] = null
+            dispatch({ type: 'SET_LOGIN', payload: false })
+      })
+  }
+
+  AsyncStorage.getItem("remember_cookie").then(function(data) {
+    if (data != null) {
+        axios.defaults.headers.common["cookie"] = data
+        dispatch({ type: 'SET_LOGIN', payload: true })
+    }
+  })
 
   return (
     <NavigationContainer ref={navigationRef}>
@@ -29,7 +46,14 @@ export default function CompleteFlow () {
         )
       : (
 
-      <Drawer.Navigator>
+      <Drawer.Navigator drawerContent={(props) => {
+      return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <DrawerItem label="Logout" onPress={logout} />
+        </DrawerContentScrollView>
+      );
+    }}>
         <Drawer.Screen name="Input" component={BasicInputFlow} options={{ title: 'Registrar' }} />
         <Drawer.Screen name="Edit" component={EditFlow} options={{ title: 'Editar' }} />
         <Drawer.Screen name="Metas" component={GoalsFlow} options={{ title: 'Metas' }} />
