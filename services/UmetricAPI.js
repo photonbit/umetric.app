2aa90931a281adc1f8e11e7626a5053da9100d35
Daniel Moreno <daniel.moreno@tesselo.com>
2021-11-28 15:14:21 +0000

    feat: store login cookie

--- services/UmetricAPI.js
+++ services/UmetricAPI.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useContext} from "react";
+import {Context} from "../filters/Store";
+
 
 let UmetricAPI = axios.create({
     baseURL: 'https://umetric.es',
     timeout: 10000,
 });
 
-
 export async function login(username, password) {
     const response = await UmetricAPI.post("/api/auth",
         {"username": username, "password": password}
     )
 
+    let cookies = response.headers["set-cookie"]
+    let remember_cookie
+
+    if (cookies) {
+        cookies.forEach(function(cookie) {
+            if (cookie.startsWith("remember_token")) {
+                remember_cookie = cookie
+            }
+        })
+
+        if (remember_cookie) {
+            await AsyncStorage.setItem('remember_cookie', remember_cookie)
+        }
+    }
     return response
 }
 
