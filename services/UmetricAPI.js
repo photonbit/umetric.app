1cdba30b1399fa24f863a2db96ca03cd38b4d2bb
Daniel Moreno <dani@photonbit.es>
2022-05-04 17:08:05 +0100

    chore(eslint): lint UmetricAPI service

--- services/UmetricAPI.js
+++ services/UmetricAPI.js
@@ -1,56 +1,55 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const UmetricAPI = axios.create({
+  baseURL: 'https://umetric.es',
+  timeout: 10000
+})
 
-let UmetricAPI = axios.create({
-    baseURL: 'https://umetric.es',
-    timeout: 10000,
-});
+export async function login (username, password) {
+  try {
+    const response = await UmetricAPI.post('/api/auth',
+      { username: username, password: password }
+    )
 
-export async function login(username, password) {
-    try {
-        const response = await UmetricAPI.post("/api/auth",
-            {"username": username, "password": password}
-        )
+    const cookies = response.headers['set-cookie']
+    let remember_cookie
 
-        let cookies = response.headers["set-cookie"]
-        let remember_cookie
-
-        if (cookies) {
-            cookies.forEach(function (cookie) {
-                if (cookie.startsWith("remember_token")) {
-                    remember_cookie = cookie
-                }
-            })
-
-            if (remember_cookie) {
-                await AsyncStorage.setItem('remember_cookie', remember_cookie)
-            }
+    if (cookies) {
+      cookies.forEach(function (cookie) {
+        if (cookie.startsWith('remember_token')) {
+          remember_cookie = cookie
         }
-        return response
-    } catch (e) {
-        console.log("login: " + e.message)
+      })
+
+      if (remember_cookie) {
+        await AsyncStorage.setItem('remember_cookie', remember_cookie)
+      }
     }
+    return response
+  } catch (e) {
+    console.log('login: ' + e.message)
+  }
 }
 
-export async function getCategories() {
-    try {
-        const {data} = await UmetricAPI.get("/api/categories")
+export async function getCategories () {
+  try {
+    const { data } = await UmetricAPI.get('/api/categories')
 
-        return data
-    } catch (e) {
-        console.log("getCategories: " + e.message)
-        return []
-    }
+    return data
+  } catch (e) {
+    console.log('getCategories: ' + e.message)
+    return []
+  }
 }
 
-export async function addCategory(category) {
-    try {
-        return await UmetricAPI.post("/api/categories", category)
-    } catch (e) {
-        console.log("addCateory(" + category + "): " + e.message)
-        return null
-    }
+export async function addCategory (category) {
+  try {
+    return await UmetricAPI.post('/api/categories', category)
+  } catch (e) {
+    console.log('addCateory(' + category + '): ' + e.message)
+    return null
+  }
 }
 
 export async function editCategory({categoryId, modifiedCategory}) {
@@ -62,52 +61,51 @@ export async function editCategory({categoryId, modifiedCategory}) {
     }
 }
 
-export async function getIcons() {
-    try {
-        const {data} = await UmetricAPI.get("/api/icons")
+export async function getIcons () {
+  try {
+    const { data } = await UmetricAPI.get('/api/icons')
 
-        return data
-    } catch (e) {
-        console.log("getIcons(): " + e.message)
-        return []
-    }
+    return data
+  } catch (e) {
+    console.log('getIcons(): ' + e.message)
+    return []
+  }
 }
 
-export async function getIcon({queryKey}) {
-    const icon = queryKey[1]
-    try {
-        if (icon === undefined) {
-            return null
-        }
-        const {data} = await UmetricAPI.get("/static/" + icon)
-
-        return data
-    } catch (e) {
-        console.log("getIcon(" + icon + "): " + e.message)
-        return null
+export async function getIcon ({ queryKey }) {
+  const icon = queryKey[1]
+  try {
+    if (icon === undefined) {
+      return null
     }
+    const { data } = await UmetricAPI.get('/static/' + icon)
+
+    return data
+  } catch (e) {
+    console.log('getIcon(' + icon + '): ' + e.message)
+    return null
+  }
 }
 
-
-export async function addEvent({categoryId, newEvent}) {
-    try {
-        return await UmetricAPI.post("/api/categories/" + categoryId + "/events", newEvent)
-    } catch (e) {
-        console.log("addEvent("+categoryId+"): " + e.message)
-        console.log(newEvent)
-        return null
-    }
+export async function addEvent ({ categoryId, newEvent }) {
+  try {
+    return await UmetricAPI.post('/api/categories/' + categoryId + '/events', newEvent)
+  } catch (e) {
+    console.log('addEvent(' + categoryId + '): ' + e.message)
+    console.log(newEvent)
+    return null
+  }
 }
 
-export async function getEvent({queryKey}) {
-    const event_id = queryKey[1]
-    try {
-        const {data} = await UmetricAPI.get("/api/events/" + event_id)
-        return data
-    } catch (e) {
-        console.log("getEvent(" + event_id + "): " + e.message)
-        return null
-    }
+export async function getEvent ({ queryKey }) {
+  const event_id = queryKey[1]
+  try {
+    const { data } = await UmetricAPI.get('/api/events/' + event_id)
+    return data
+  } catch (e) {
+    console.log('getEvent(' + event_id + '): ' + e.message)
+    return null
+  }
 }
 
 export async function editEvent({eventId, modifiedEvent}) {
@@ -130,15 +128,15 @@ export async function getCategory({queryKey}) {
     }
 }
 
-export async function getEvents({queryKey}) {
-    const category_id = queryKey[1]
-    try {
-        const {data} = await UmetricAPI.get("/api/categories/" + category_id + "/events")
-        return data
-    } catch (e) {
-        console.log("getEvents(" + category_id + "): " + e.message)
-        return []
-    }
+export async function getEvents ({ queryKey }) {
+  const category_id = queryKey[1]
+  try {
+    const { data } = await UmetricAPI.get('/api/categories/' + category_id + '/events')
+    return data
+  } catch (e) {
+    console.log('getEvents(' + category_id + '): ' + e.message)
+    return []
+  }
 }
 
 export default UmetricAPI;
