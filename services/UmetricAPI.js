29e65cbb9e40bcbd95bea773ed82acbe50157a10
Daniel Moreno <dani@photonbit.es>
2022-05-04 17:09:26 +0100

    feature(api): handle goals and commitments

--- services/UmetricAPI.js
+++ services/UmetricAPI.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const UmetricAPI = axios.create({
   baseURL: 'https://umetric.es',
@@ -170,4 +170,68 @@ export async function getEvents ({ queryKey }) {
   }
 }
 
-export default UmetricAPI;
+export async function getGoals () {
+  try {
+    const { data } = await UmetricAPI.get('/api/goals')
+    return data
+  } catch (e) {
+    console.log('getGoals(): ' + e.message)
+    return []
+  }
+}
+
+export async function getGoal ({ queryKey }) {
+  const goal_id = queryKey[1]
+  try {
+    const { data } = await UmetricAPI.get('/api/goals/' + goal_id)
+    return data
+  } catch (e) {
+    console.log('getGoal(' + goal_id + '): ' + e.message)
+    return null
+  }
+}
+
+export async function addGoal ({ newGoal }) {
+  try {
+    return await UmetricAPI.post('/api/goals', newGoal)
+  } catch (e) {
+    console.log('addGoal(): ' + e.message)
+    console.log(newGoal)
+    return null
+  }
+}
+
+export async function editGoal ({ goalId, modifiedGoal }) {
+  try {
+    return await UmetricAPI.put('/api/goals/' + goalId, modifiedGoal)
+  } catch (e) {
+    console.log('editGoal(' + goalId + '): ' + e.message)
+    return null
+  }
+}
+
+export async function deleteGoal ({ goalId }) {
+  try {
+    return await UmetricAPI.delete('/api/goals/' + goalId)
+  } catch (e) {
+    console.log('deleteGoal(' + goalId + '): ' + e.message)
+    return null
+  }
+}
+
+export async function getCommitments ({ queryKey }) {
+  const week = queryKey[1]
+  let url = '/api/commitments/'
+  if (parseInt(week) > 0) {
+    url += week
+  }
+  try {
+    const { data } = await UmetricAPI.get(url)
+    return data
+  } catch (e) {
+    console.log('getCommitments(' + week + '): ' + e.message)
+    return []
+  }
+}
+
+export default UmetricAPI
