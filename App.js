b43d03b3be2b5bbab4d011b43347882723d1f3db
Daniel Moreno <dani@photonbit.es>
2022-05-04 17:03:49 +0100

    feat(instrumentation): add sentry plugin

--- App.js
+++ App.js
@@ -18,13 +18,20 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { LogBox } from 'react-native'
+import * as Sentry from 'sentry-expo'
 
 import CompleteFlow from './navigation/CompleteFlow'
 import Store from './filters/Store'
 
+Sentry.init({
+  dsn: 'https://e7fa608dd74142ce8a1817f4ea79ad37@o556434.ingest.sentry.io/5691192',
+  enableInExpoDevelopment: true,
+  debug: true
+})
+
 LogBox.ignoreLogs(['Setting a timer'])
 
-export default function App () {
+const App = () => {
   const queryClient = new QueryClient()
 
   return (
@@ -35,3 +42,5 @@ export default function App () {
       </QueryClientProvider>
   )
 }
+
+export default Sentry.Native.wrap(App)
