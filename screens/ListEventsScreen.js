7ec291ca6ef9398b275bc2566f8547690056aa28
Daniel Moreno <dani@photonbit.es>
2022-05-04 17:26:00 +0100

    feat(events): use the new call to log events

--- screens/ListEventsScreen.js
+++ screens/ListEventsScreen.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import * as Linking from 'expo-linking'
 
 import Element from '../components/Element'
 
-import { getEvents } from '../services/UmetricAPI'
+import { getEvents, logEvent } from '../services/UmetricAPI'
+import * as RootNavigation from "../navigation/RootNavigation";
 
 export default function ListCategoriesScreen ({ route }) {
-  const onPress = (item) => Linking.openURL(item.action)
   const categoryId = route.params.category_id
-
+  const [event, setEvent] = useState({
+    id: '',
+    action: '',
+    name: '',
+    icon: ''
+  })
   const { data, error, isError, isLoading } = useQuery(['events', categoryId], getEvents)
+  const mutation = useMutation((eventId) => logEvent({ eventId: event.id }))
+  const { isSuccess } = mutation
+
+  const onPress = (item) => {
+    setEvent(item)
+    mutation.mutate(event.id)
+  }
+
+  const renderItem = ({ item }) => (
+    <Element
+    element={item}
+    onPress={() => onPress(item)} />
+  )
+
+  useEffect(() => {
+    if (isSuccess) {
+      if (event.action) {
+        Linking.openURL(event.action)
+      }
+
+      RootNavigation.navigate('ListCategories')
+    }
+  }, [isSuccess])
 
   if (isLoading) {
     return <View><Text>Loading...</Text></View>
@@ -20,11 +48,6 @@ export default function ListCategoriesScreen ({ route }) {
     return <View><Text>Something is wrong: {error.message}...</Text></View>
   }
 
-  const renderItem = ({ item }) => (
-    <Element
-    element={item}
-    onPress={() => onPress(item)} />
-  )
   return (
           <FlatList
           style={styles.flatlist}
