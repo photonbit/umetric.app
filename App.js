1774d08a107f039ba6a53566e2a47b186cf46ad8
Daniel Moreno <dani@photonbit.es>
2025-02-09 09:12:48 +0000

    chore: remove sentry

--- App.js
+++ App.js
@@ -19,7 +19,6 @@ import 'react-native-gesture-handler'
 import React, { useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { LogBox, Linking } from 'react-native'
-import * as Sentry from 'sentry-expo'
 import * as Localization from 'expo-localization'
 import i18n from 'i18n-js'
 import urlParse from 'url-parse'
@@ -30,14 +29,6 @@ import CompleteFlow from './navigation/CompleteFlow'
 import * as RootNavigation from './navigation/RootNavigation'
 import Store from './filters/Store'
 import { en, es, pt, jp, zh, ru, ph, de } from './i18n/supportedLanguages'
-
-Logs.enableExpoCliLogging()
-
-Sentry.init({
-  dsn: 'https://e7fa608dd74142ce8a1817f4ea79ad37@o556434.ingest.sentry.io/5691192',
-  enableInExpoDevelopment: true,
-  debug: true
-})
 
 i18n.fallbacks = true
 i18n.translations = { en, es, pt, jp, zh, ru, ph, de }
@@ -83,4 +74,4 @@ const App = () => {
   )
 }
 
-export default Sentry.Native.wrap(App)
+export default App;
