4f91544d9d2c376e3ea35f07e76961d7ed156d2a
Daniel Moreno <dani@photonbit.es>
2025-02-09 09:11:56 +0000

    fix: adap edit categories to new API

--- screens/EditListCategoriesScreen.js
+++ screens/EditListCategoriesScreen.js
@@ -20,9 +20,9 @@ export default function EditListCategoriesScreen ({ navigation }) {
   }
 
   const deleteMutation = useMutation(
-    (categoryId) => deleteCategory({ categoryId }))
+    (categoryId) => deleteCategory(categoryId))
   const orderMutation = useMutation(
-      (categories) => updateCategories({categories})
+      (categories) => updateCategories(categories)
   )
   const isDeleteSuccess = deleteMutation.isSuccess
   const isOrderSuccess = orderMutation.isSuccess
